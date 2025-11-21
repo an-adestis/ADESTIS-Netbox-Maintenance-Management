9c9c248cb0fcef2661635117ae7dee8802d44572
amin nabiyev <a.nabiyev@adestis.de>
2025-11-21 12:59:09 +0100

    einfügen von plans und status

---
+++ adestis_netbox_maintenance_management/filtersets/maintenanceplannedactions.py
@@ -0,0 +1,54 @@
+from adestis_netbox_maintenance_management.models import MaintenanceActions
+from adestis_netbox_maintenance_management.models import MaintenancePlannedActions
+from netbox.filtersets import NetBoxModelFilterSet
+
+from django.db.models import Q
+from django.utils.translation import gettext as _
+
+from utilities.forms.fields import (
+    DynamicModelMultipleChoiceField,
+)
+import django_filters
+from utilities.filters import TreeNodeMultipleChoiceFilter
+from virtualization.models import *
+from tenancy.models import *
+from dcim.models import *
+from ipam.api.serializers import *
+from ipam.api.field_serializers import *
+
+
+__all__ = (
+    'MaintenancePlannedActionsFilterSet',
+)
+
+class MaintenancePlannedActionsFilterSet(NetBoxModelFilterSet):
+    
+    tenant_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=Tenant.objects.all(),
+        label=_('Tenant (ID)'),
+    )
+    
+    tenant = django_filters.ModelMultipleChoiceFilter(
+        queryset=Tenant.objects.all(),
+        required=False,
+        field_name='tenant__name',
+        to_field_name='tenant',
+        label=_('Tenant (name)'),
+    )
+    
+    maintenance_action = django_filters.ModelMultipleChoiceFilter(
+        queryset=MaintenanceActions.objects.all(),
+        required = False,
+        field_name='maintenance_action',
+        label=_('Maintenance Action (name)'),
+    )
+    
+    class Meta:
+        model = MaintenancePlannedActions
+        fields = ['id', 'name', 'maintenance_action', 'tenant']
+    
+
+    def search(self, queryset, name, value):
+        if not value.strip():
+            return queryset
+
